9210a629d6fc2c27735c49d631b7061afa5223d5
McStephen-Core <nehpetscott@mcstephen.com>
2025-07-08 13:31:52 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        jade: '#00A86B' // MCSTEPHEN Jade Green
+      }
+    }
+  },
+  plugins: [],
+}
